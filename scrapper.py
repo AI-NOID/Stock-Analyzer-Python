ad022611e37eeb62ddd4b4a517280fa0844a4b9e
Sardi Irfansyah <64241627+Sardiirfan27@users.noreply.github.com>
2023-05-12 06:25:40 +0700

    Update3 scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
+import chromedriver_binary
 
 import time
 import pandas as pd
